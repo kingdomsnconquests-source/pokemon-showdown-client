dff3d233f887af89432dbbde57cd5a005f7c78c2
ImTotallyNotOnGitHub <franzandrew16@gmail.com>
2025-12-27 11:54:13 -0500

    Update replay-embed.template.js

--- play.pokemonshowdown.com/js/replay-embed.template.js
+++ play.pokemonshowdown.com/js/replay-embed.template.js
@@ -45,9 +45,9 @@ requireScript('https://play.pokemonshowdown.com/data/pokedex.js?a7');
 requireScript('https://play.pokemonshowdown.com/data/moves.js?a7');
 requireScript('https://play.pokemonshowdown.com/data/abilities.js?a7');
 requireScript('https://play.pokemonshowdown.com/data/items.js?a7');
-requireScript('client.kingdomsnconquests.dynv6.net/data/teambuilder-tables.js?a7');
-requireScript('client.kingdomsnconquests.dynv6.net/js/battle-tooltips.js?a7');
-requireScript('client.kingdomsnconquests.dynv6.net/js/battle.js?a7');
+requireScript('https://play.pokemonshowdown.com/data/teambuilder-tables.js?a7');
+requireScript('https://play.pokemonshowdown.com/js/battle-tooltips.js?a7');
+requireScript('https://play.pokemonshowdown.com/js/battle.js?a7');
 
 var Replays = {
 	battle: null,
